e655599f4c1dfbcf4c76a6a5106da577f250b3ca
Dev_Palsanawala <devanandp2000@gmail.com>
2024-07-15 17:08:14 +0530

    All Done Now!

--- todoapp/lib/src/constants/api.dart
+++ todoapp/lib/src/constants/api.dart
@@ -1,8 +1,8 @@
 // const String url = "http://10.0.2.2:2000"; // for emulator
 const String url =
     "http://192.168.29.115:2000"; //your pc & laptop ipv4 address for running on your physical mobile device
-const String registrationurl = "$url/registration";
-const String loginurl = "$url/login";
-const String addToDourl = "$url/todoCreate";
-const String getuserToDourl = "$url/getTodo";
-const String deleteToDourl = "$url/deleteTodo";
+const String registrationurl = "$url/registration"; // for Registration
+const String loginurl = "$url/login"; // for login
+const String addToDourl = "$url/todoCreate"; // for adding todo
+const String getuserToDourl = "$url/getTodo"; // for getting todo
+const String deleteToDourl = "$url/deleteTodo";// for deleting todo
